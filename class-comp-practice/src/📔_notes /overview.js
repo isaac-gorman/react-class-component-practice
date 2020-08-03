fea3481fdd23cd7cc08e306c57f2228ed36b9ed7
Isaac Gorman <emailme@isaacg.net>
2020-08-03 08:34:01 -0500

    overview notes complete

---
+++ class-comp-practice/src/📔_notes /overview.js
@@ -0,0 +1,12 @@
+// Overview
+// Q:What will I cover today?
+// - I will learn how to use classes to build react components
+// Q: Why are react class components important to the react ecosystem?
+// - Class components brought a lot of functionality that wasn't there before with the original React API when using function components.
+// - However it is now available with hook components. But this is a new feature to the react ecosystem.
+// - So learning class componenents is impornatant because the majory of React code is written with class components.
+
+// Learning Objectives
+// - I should be able to explain what class components are. While being able to use class components to render state data to DOM.
+// - I should be able to share data between class components using state and props.
+// - I should also be able to respond to events triggred by user interactions, while being able to handle user inputs via forms in React.
