09cffff74e45e359296bd45f4673344af1d00ce0
Isaac Gorman <emailme@isaacg.net>
2020-08-03 10:25:49 -0500

    rendering data to DOM using class components follow along is complete. Up next => Sharing data and state using props

--- class-comp-practice/src/App.js
+++ class-comp-practice/src/App.js
@@ -14,7 +14,12 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      message:
+        "Only when you work through the fear, and confusion comes progress!!",
+      prayer:
+        "God please protect others, and just give me the oppurtunity to face my fears",
+    };
   }
   render() {
     return (
@@ -23,6 +28,9 @@ class App extends React.Component {
           <h3>React Class Components</h3>
         </header>
         <h1>Hi!</h1>
+        <div>{this.state.message}</div>
+        <br />
+        <div>{this.state.prayer}</div>
       </div>
     );
   }
