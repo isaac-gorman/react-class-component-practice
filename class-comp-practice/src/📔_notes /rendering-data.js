b755e57a31a6bebae6c51a210af7eb1f9507bd96
Isaac Gorman <emailme@isaacg.net>
2020-08-03 08:59:05 -0500

    intro to component notes completed

---
+++ class-comp-practice/src/📔_notes /rendering-data.js
@@ -0,0 +1,23 @@
+// Objective
+// - I need to be able to explain what a react class component is.
+// - Use a class comp to render some state data to the DOM
+
+// Q: What is Component?
+// - It is a constructor class that comes with React that has some very usefull properties on it.
+// - The class component is just like any other class I have come across in vinilla js.
+// - The majority of the class components abilities comes from using the single 'base Component'
+// - Using the singe 'base Compontent' is what allows dev's to scale out completely stactic component based web pages to become fully functional web applications.
+
+// Q: What did React give us the idea for?
+
+// Q: What is the outcome of setting up a component as a class?
+
+// Q: How do you declare a class component?
+
+// Q: What is a good way to remember how to render a class component?
+
+// Q: What are the capabilites of a dev once the basic layout of the class components is created?
+
+// Q: What are the benifit that come built in with the React API?
+
+// Q: What will be our main focus for this lesson with React?
